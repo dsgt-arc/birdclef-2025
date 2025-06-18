9c6b68a2af49a6fdf3e30e8efd14a2e70d64503f
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2025-06-18 04:52:57 -0400

    Add final inference script for mel2vec

--- birdclef/mel2vec/loaders.py
+++ birdclef/mel2vec/loaders.py
@@ -8,7 +8,7 @@ from gensim.models import KeyedVectors
 @cache
 def get_index(path):
     """Get the FAISS index for the centroids."""
-    centroids = np.load(path)
+    centroids = np.load(str(path))
     index = faiss.IndexFlatL2(centroids.shape[1])
     index.add(centroids)
     return index
@@ -17,10 +17,10 @@ def get_index(path):
 @cache
 def get_word_vectors(path):
     """Get the word vectors from the Word2Vec model."""
-    return KeyedVectors.load(path)
+    return KeyedVectors.load(str(path))
 
 
 @cache
 def get_pca(path):
     """Get the PCA model from the tokenizer."""
-    return faiss.read_VectorTransform(path)
+    return faiss.read_VectorTransform(str(path))
